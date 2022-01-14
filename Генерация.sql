66321bec623683f910888417bb3cd4b9106ca95b
Evgeny <44237535+Dakera@users.noreply.github.com>
2022-01-14 17:37:45 +0700

    Add files via upload

---
+++ Генерация.sql
@@ -0,0 +1,73 @@
+CREATE TABLE help -- Вспомогательная таблица
+(  adress text
+	
+);
+
+
+-- Больницы (что такое phones?)
+DO $$
+DECLARE
+	smth varchar(100);
+	my_name text;
+	my_adress text; 
+	my_phone text;
+BEGIN
+	FOR i IN 1 .. 6
+	LOOP
+		SELECT CONCAT('Городская больница №', CAST(ROUND(random()*14+1) AS text)) INTO my_name;
+		SELECT CONCAT(adress, ', ', ROUND(random()*(111-1)+1)) INTO my_adress
+		FROM help WHERE random() > 0.92 LIMIT 1;
+		SELECT CONCAT('+73822', CAST(FLOOR(random()*(1000000-100000)+100000) AS text)) INTO my_phone;
+		INSERT INTO hospitals (name, address, phone)
+		VALUES(my_name, my_adress, my_phone);
+	END LOOP;
+END $$;
+
+select * from hospitals;
+delete from hospitals;
+
+------------------------------------------------------------------------------------------------------------
+CREATE TABLE "Даты" -- Вспомогательная таблица с датами
+(  "Даты_и_время" TIMESTAMP WITHOUT TIME ZONE NOT NULL
+ , "Даты" DATE NOT NULL
+ , "Время" TIME WITHOUT TIME ZONE NOT NULL
+);
+
+-- Даты (надо подправить, полностью скопировал с первой индивидуалки своей)
+DO $$
+DECLARE
+	day1 INTEGER;
+	smth varchar(100);
+	my_date TIMESTAMP WITHOUT TIME ZONE;
+	my_d DATE;
+	my_t TIME WITHOUT TIME ZONE;
+BEGIN
+	FOR i IN 1 .. 60
+	LOOP
+		SELECT (random() + 11) INTO day1;
+		SELECT make_timestamp(2021, CAST(random() + 11 AS INTEGER), CAST((random()*29 + 1) AS INTEGER), CAST(random()*24 AS INTEGER), 0, 0) INTO my_date;
+		SELECT make_date(2021, CAST(random() + 11 AS INTEGER), CAST((random()*29 + 1) AS INTEGER)) INTO my_d;
+		SELECT make_time(CAST(random()*24 AS INTEGER), 0, 0) INTO my_t;
+		-- Через селект в инсерте почему-то не работает (пустой результат)
+		--RAISE NOTICE '%', my_date;
+		INSERT INTO "Даты" ("Даты_и_время", "Даты", "Время")
+		VALUES(my_date, my_d, my_t);
+	END LOOP;
+END $$;
+
+---------------------------------------------------------------------------------------------------------------
+CREATE TABLE russian_names (
+	 id INT NOT NULL,
+	 Name VARCHAR(100) NOT NULL,
+	 Sex VARCHAR(1) NULL,
+	 PeoplesCount INT NULL,
+	 WhenPeoplesCount TIMESTAMP NULL,
+);
+
+CREATE TABLE russian_surnames (
+	 id INT NOT NULL,
+	 Surname VARCHAR(100) NOT NULL,
+	 Sex VARCHAR(1) NULL,
+	 PeoplesCount INT NULL,
+	 WhenPeoplesCount TIMESTAMP NULL,
+);
