8be5b8e1405da2510faf41fdd6d7c656a576148c
Evgeny <44237535+Dakera@users.noreply.github.com>
2022-01-14 17:43:11 +0700

    Add files via upload

---
+++ объявление и заполнение help.sql
@@ -0,0 +1,216 @@
+CREATE TABLE help -- Вспомогательная таблица
+(  adress text
+ 
+);
+INSERT INTO help (adress) 
+VALUES ('улица Азербайджанская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Азиатская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Алтайская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Алеутская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Баумана');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Албанский');
+
+INSERT INTO help (adress) 
+VALUES ('улица Алексея Беленца');
+
+INSERT INTO help (adress) 
+VALUES ('улица Бердская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Пушкина');
+
+INSERT INTO help (adress) 
+VALUES ('проспект Ленина');
+
+INSERT INTO help (adress) 
+VALUES ('проезд Вилюйский');
+
+INSERT INTO help (adress) 
+VALUES ('улица Войкова');
+
+INSERT INTO help (adress) 
+VALUES ('улица Герасименко');
+
+INSERT INTO help (adress) 
+VALUES ('улица Енисейская');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Живописный');
+
+INSERT INTO help (adress) 
+VALUES ('улица Жуковского');
+
+INSERT INTO help (adress) 
+VALUES ('улица Ивана Черных');
+
+INSERT INTO help (adress) 
+VALUES ('улица Карпова');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Карповский');
+
+INSERT INTO help (adress) 
+VALUES ('улица Клюева');
+
+INSERT INTO help (adress) 
+VALUES ('проспект Комсомольский');
+
+INSERT INTO help (adress) 
+VALUES ('улица Малиновая');
+
+INSERT INTO help (adress) 
+VALUES ('улица Мирная');
+
+INSERT INTO help (adress) 
+VALUES ('проспект Мира');
+
+INSERT INTO help (adress) 
+VALUES ('проспект Кирова');
+
+INSERT INTO help (adress) 
+VALUES ('улица Народная');
+
+INSERT INTO help (adress) 
+VALUES ('улица Кулева');
+
+INSERT INTO help (adress) 
+VALUES ('улица Молодежная');
+
+INSERT INTO help (adress) 
+VALUES ('улица Нахимова');
+
+INSERT INTO help (adress) 
+VALUES ('улица Невская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Омская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Нефтяная');
+
+INSERT INTO help (adress) 
+VALUES ('улица Осипова');
+
+INSERT INTO help (adress) 
+VALUES ('улица Победы');
+
+INSERT INTO help (adress) 
+VALUES ('улица Полярная');
+
+INSERT INTO help (adress) 
+VALUES ('улица Прибрежная');
+
+INSERT INTO help (adress) 
+VALUES ('улица Таежная');
+
+INSERT INTO help (adress) 
+VALUES ('улица Томская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Сосновая');
+
+INSERT INTO help (adress) 
+VALUES ('улица Усова');
+
+INSERT INTO help (adress) 
+VALUES ('улица Трудовая');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Строительный');
+
+INSERT INTO help (adress) 
+VALUES ('проспект Фрунзе');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Цветочный');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Шегарский');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Успенского');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Энергетический');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Ясный');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Чудесный');
+
+INSERT INTO help (adress) 
+VALUES ('улица Хабаровская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Харьковская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Студенческая');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Удачи');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Тихий');
+
+INSERT INTO help (adress) 
+VALUES ('улица Тверская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Крылова');
+
+INSERT INTO help (adress) 
+VALUES ('улица Кривая');
+
+INSERT INTO help (adress) 
+VALUES ('улица Котовского');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Мирный');
+
+INSERT INTO help (adress) 
+VALUES ('проезд Кузнечный');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Кузнецкий');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Новый');
+
+INSERT INTO help (adress) 
+VALUES ('улица Кутузова');
+
+INSERT INTO help (adress) 
+VALUES ('улица Красноярская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Киевская');
+
+INSERT INTO help (adress) 
+VALUES ('улица Лазарева');
+
+INSERT INTO help (adress) 
+VALUES ('переулок Зеленый');
+
+INSERT INTO help (adress) 
+VALUES ('Иркутский тракт');
+
+INSERT INTO help (adress) 
+VALUES ('Московский тракт');
+
+INSERT INTO help (adress) 
+VALUES ('Богашевский Тракт');
+
+INSERT INTO help (adress) 
+VALUES ('Кузовлевский Тракт');
