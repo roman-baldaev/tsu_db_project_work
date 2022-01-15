7c1d337a5fd7c261f120c65d10f31efc9501324e
Evgeny <3dakera@gmail.com>
2022-01-15 13:19:13 +0700

    Самописная база отчеств

---
+++ data/russian_patronymics_data.sql
@@ -0,0 +1,104 @@
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Алексеевич', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Алексеевна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Владимирович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Владимировна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Николаевич', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Николевна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Евгеньевич', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Евгеньевна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Романович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Романова', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Максимович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Максимовна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Денисович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Денисовна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Олегович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Олеговна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Климович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Климовна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Артемович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Артемовна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Юрьевич', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Юрьева', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Вячеславович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Вячеславовна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Михайлович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Михайловна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Егорович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Егоровна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Ильич', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Тимурович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Тимуровна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Павлович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Павловна', 'Ж');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Владиславович', 'М');
+
+INSERT INTO russian_patronymics (patronymic, sex)
+VALUES('Владиславовна', 'Ж');
